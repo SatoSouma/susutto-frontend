6dd4f92276a634018bc07e8b40875e07905f26aa
sato-souma <59986133+sato-souma@users.noreply.github.com>
2021-12-13 23:14:45 +0900

    miss fix

---
+++ pages/employee.tsx
@@ -0,0 +1,12 @@
+import React, { VFC } from 'react';
+import { Employee } from '../public/src/components/entory_point';
+
+const Employee_page: VFC = () => {
+  return (
+    <>
+      <Employee />
+    </>
+  );
+};
+
+export default Employee_page;
---
+++ public/src/components/templates/Employee/Employee.tsx
@@ -0,0 +1,12 @@
+import React, { VFC } from 'react';
+import { AlltaskBtn } from '../../entory_point';
+
+const Employee = () => {
+  return (
+    <>
+      <AlltaskBtn />
+    </>
+  );
+};
+
+export default Employee;
