e3dc5b02b8660556f272f6da8cf5faeb90cd7161
sato-souma <59986133+sato-souma@users.noreply.github.com>
2021-12-18 17:06:23 +0900

    task list fix-summary

--- public/src/components/atoms/TaskList/TaskList.tsx
+++ public/src/components/atoms/TaskList/TaskList.tsx
@@ -5,16 +5,10 @@ import styles from './TaskList.module.scss';
 const TaskList: VFC = () => {
   return (
     <div className={styles.tasklist}>
-      <Button
-        height="70px"
-        width="72"
-        bg="orange.100"
-        border="Yellow.100"
-        color="black"
-        borderColor="orange.300"
-      >
-        お仕事
-      </Button>
+      <details className={styles.details}>
+        <summary className={styles.tasks}>summaryタグで指定した要約</summary>
+        <p>details要素に含まれる詳細内容。</p>
+      </details>
     </div>
   );
 };
